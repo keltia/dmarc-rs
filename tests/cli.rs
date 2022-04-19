49a85dd508ed419209bb02e15d8c0adb4e5b07f6
Ollivier Robert <roberto@keltia.net>
2022-04-19 19:54:34 +0200

    Refactor: all CLI tests into cli.rs.

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,6 +2,16 @@
 use assert_cmd::Command;
 
 #[test]
+fn test_help() {
+    let mut cmd = Command::cargo_bin("dmarc-cat").unwrap();
+    let assert = cmd
+        .arg("-h")
+        .assert();
+    assert
+        .success();
+}
+
+#[test]
 fn test_version() {
     let mut cmd = Command::cargo_bin("dmarc-cat").unwrap();
     println!("{:?}", cmd);
