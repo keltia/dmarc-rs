c7bda8b033191f0f4b93f4f980bd4bb8aa3c6d58
Ollivier Robert <roberto@keltia.net>
2022-04-20 23:08:27 +0200

    Fix documentation prefixes, links, etc.

--- src/bin/dmarc-cat/main.rs
+++ src/bin/dmarc-cat/main.rs
@@ -5,29 +5,32 @@
 //! Should work properly on UNIX (FreeBSD, Linux, etc.) and Windows systems.
 //!
 //! ## Columns
-//
-// The full XML grammar is available [here](https://tools.ietf.org/html/rfc7489#appendix-C)
-//
-// The report has several columns:
-//
-// - `IP` is matching IP address
-// - `Count` is the number of times this IP was present
-// - `From` is the `From:` header value
-// - `RFrom` is the envelope `From` value
-// - `RDKIM` is the result from DKIM checking
-// - `RSPF` is the result from SPF checking
-
+//!
+//! The full XML grammar is available here: [dmarc.xsd](https://tools.ietf.org/html/rfc7489#appendix-C)
+//! (for your convenience, a local copy is the `doc/` directory in the repository.
+//!
+//! The report has several columns:
+//!
+//! - `IP` is matching IP address
+//! - `Count` is the number of times this IP was present
+//! - `From` is the `From:` header value
+//! - `RFrom` is the envelope `From` value
+//! - `RDKIM` is the result from DKIM checking
+//! - `RSPF` is the result from SPF checking
+//!
 //! ## Notes
 //!
-//! The package is still named `dmarc-rs` to distinguish it from the [Go] version
+//! The package is still named `dmarc_rs` to distinguish it from the [Go] version
 //! but the binary will remain the same (`dmarc-cat`) and can totally replace it.
 //!
 //! ## References
-//
+//!
 //! - [DMARC](https://dmarc.org/)
+//! - [DMARC RFC](https://tools.ietf.org/html/rfc7489)
 //! - [SPF](http://www.rfc-editor.org/info/rfc7208)
 //! - [DKIM](http://www.rfc-editor.org/info/rfc6376)
-//! - [Go](https://golang.org/)
+//!
+//! [Go]: https://golang.org/
 
 // Internal crates
 //
