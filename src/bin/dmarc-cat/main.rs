705c25f7e3667477d8d2ae244f9a302d75f806e6
Ollivier Robert <roberto@keltia.net>
2022-04-20 20:20:52 +0200

    More documentation (WIP).

--- src/bin/dmarc-cat/main.rs
+++ src/bin/dmarc-cat/main.rs
@@ -1,5 +1,22 @@
 //! This is the dmarc-cat utility.
 //!
+//! `dmarc-cat` is a small command-line utility to analyze and display in a usable manner
+//! the content of the DMARC XML reports sent by the various email providers around the globe.
+//! Should work properly on UNIX (FreeBSD, Linux, etc.) and Windows systems.
+//!
+//! ## Columns
+//
+// The full XML grammar is available [here](https://tools.ietf.org/html/rfc7489#appendix-C)
+//
+// The report has several columns:
+//
+// - `IP` is matching IP address
+// - `Count` is the number of times this IP was present
+// - `From` is the `From:` header value
+// - `RFrom` is the envelope `From` value
+// - `RDKIM` is the result from DKIM checking
+// - `RSPF` is the result from SPF checking
+
 //! ## Notes
 //!
 //! The package is still named `dmarc-rs` to distinguish it from the [Go] version
