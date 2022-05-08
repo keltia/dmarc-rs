5219be8e9fa9d1c77b06e1781d5d01c78df743a3
Ollivier Robert <roberto@keltia.net>
2022-05-08 15:33:46 +0200

    Use Entry as appropriate.

--- src/bin/dmarc-cat/file.rs
+++ src/bin/dmarc-cat/file.rs
@@ -11,6 +11,7 @@ use std::path::{Path, PathBuf};
 // Internal crates
 //
 use crate::analyze::analyze_file;
+use dmarc_rs::entry::Entry;
 use dmarc_rs::filetype::*;
 
 // External crates
@@ -23,14 +24,14 @@ pub fn handle_stream(fin: &dyn io::BufRead, ftype: Input) {
 
 /// Check if every file in the list and only return the list of valid ones.
 ///
-pub fn check_for_files(lfn: &[PathBuf]) -> Vec<PathBuf> {
-    let mut res: Vec<PathBuf> = vec![];
+pub fn check_for_files(lfn: &[PathBuf]) -> Vec<Entry> {
+    let mut res: Vec<Entry> = vec![];
 
     // Check for various files.
     //
     for f in lfn.iter() {
         if f.exists() || *f == PathBuf::from("-") {
-            res.push(f.to_owned());
+            res.push(Entry::new(f));
             println!("file: {:?}", f);
         } else {
             println!("Unknown file {:?}", f);
@@ -43,16 +44,16 @@ pub fn check_for_files(lfn: &[PathBuf]) -> Vec<PathBuf> {
 /// Scan the list of files and run `analyze_file()`  on each of them
 /// accumulating results.
 ///
-pub fn scan_list(lfn: &Vec<PathBuf>) -> Result<String> {
+pub fn scan_list(lfn: &Vec<Entry>) -> Result<String> {
     let mut r = vec![];
     let mut failed = vec![];
 
     for fp in lfn {
-        let mut fh = match File::open(fp) {
+        let mut fh = match File::open(&fp.p) {
             Ok(fh) => fh,
             Err(e) => {
                 log::warn!("Warning: can't open {:?}: {}", fp, e.to_string());
-                failed.push(fp.to_str().unwrap());
+                failed.push(fp.p.to_str().unwrap());
                 continue;
             }
         };
@@ -61,7 +62,7 @@ pub fn scan_list(lfn: &Vec<PathBuf>) -> Result<String> {
     if failed.is_empty() {
         return Ok(r.join("/"));
     }
-    return Err(anyhow!("{:?}", failed));
+    Err(anyhow!("{:?}", failed))
 }
 
 #[cfg(test)]
@@ -69,13 +70,13 @@ mod tests {
     use super::*;
     #[test]
     fn test_scan_list_empty() {
-        let r = scan_list(&vec![PathBuf::from("")]);
+        let r = scan_list(&vec![Entry{p: PathBuf::from(""), ft: Input::Plain}]);
         assert!(r.is_err())
     }
 
     #[test]
     fn test_scan_list_nonexistent() {
-        let r = scan_list(&vec![PathBuf::from("/nonexistent")]);
+        let r = scan_list(&vec![Entry{p: PathBuf::from("/nonexistent"), ft: Input::Plain}]);
         assert!(r.is_err())
     }
 
@@ -100,7 +101,7 @@ mod tests {
 
         let l2 = check_for_files(&l);
         assert!(!l2.is_empty());
-        assert_eq!(vec![PathBuf::from("Cargo.toml")], l2);
+        assert_eq!(vec![Entry { p: PathBuf::from("Cargo.toml"), ft: Input::Plain }], l2);
     }
 
     #[test]
@@ -114,7 +115,10 @@ mod tests {
 
         let l2 = check_for_files(&l);
         assert_eq!(2, l2.len());
-        assert_eq!(vec![PathBuf::from("-"), PathBuf::from("Cargo.toml")], l2);
+        assert_eq!(vec![
+            Entry{p: PathBuf::from("-"), ft: Input::Plain},
+            Entry{p: PathBuf::from("Cargo.toml"), ft: Input::Plain},
+        ], l2);
     }
 
 }
