d5c0f3e17f3fb587aee2e0c1c0a6f3fe64cbe1f5
Ollivier Robert <roberto@keltia.net>
2022-05-18 19:48:46 +0200

    Resolving code is out of the library for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-//! This crate implement the library part of `dmarc-rs`, dealing with IPs and list of IPs (including
-//! the resolving part of it).
+//! This crate implement the library part of `dmarc-rs`, dealing with IPs and list of IPs
 //!
 
 pub mod entry;
 pub mod filetype;
 pub mod ip;
 pub mod iplist;
-pub mod resolve;
 pub mod types;
