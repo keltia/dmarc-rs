d2f9b098eac156f82c64d4bf60519920ee7db40f
Ollivier Robert <roberto@keltia.net>
2022-11-14 00:47:52 +0100

    Add two macros: makepath! to generate a pathname and makelist! to create a Vec of pathnames.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,29 @@
 //!
 
 pub mod entry;
-pub mod filetype;
+pub mod input;
 pub mod res;
+pub mod task;
 pub mod types;
+
+/// Simple macro to generate PathBuf from a series of entries
+///
+#[macro_export]
+macro_rules! makepath {
+    ($($item:expr),+) => {
+        [
+        $(PathBuf::from($item),)+
+        ]
+        .iter()
+        .collect()
+    };
+}
+
+/// Simple macro to generate PathBuf from a series of entries
+///
+#[macro_export]
+macro_rules! makelist {
+    ($($item:expr),+) => {
+        vec![ $(PathBuf::from($item),)+ ]
+    };
+}
