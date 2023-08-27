a421b53fc6ed3faaa4048dbcfd31f8ee50d834d2
Ollivier Robert <roberto@keltia.net>
2023-08-27 23:33:03 +0200

    Add support for tar+gz.  Merge tests.

--- src/filetype.rs
+++ src/filetype.rs
@@ -19,97 +19,92 @@
 // Std Library
 //
 use std::ffi::OsStr;
+use std::fmt::Debug;
 use std::path::PathBuf;
 
 // External crates
 //
-use anyhow::{anyhow, Result};
+use clap::ValueEnum;
 
 /// Allowed type of input
 ///
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(
+    Clone, Copy, Debug, Default, Ord, PartialOrd, Eq, PartialEq, strum::Display, ValueEnum,
+)]
+#[strum(serialize_all = "lowercase")]
 pub enum Input {
     /// Plain CSV files
     Csv,
-    /// plain text files
-    Plain,
     /// XML compressed with gzip
     Gzip,
     /// Actual XML files
+    /// plain text files are assumed to be XML
+    #[default]
     Xml,
     /// ZIP files with generally both CSV and XML
     Zip,
+    /// Tar+Gz
+    TarGzip,
     /// Error type
     Unknown,
 }
 
-impl Default for Input {
-    fn default() -> Self {
-        Input::Unknown
+impl<T> From<T> for Input
+where
+    T: Into<PathBuf> + Debug + Clone,
+{
+    /// Matches a filename to a given input type based on the extension.
+    /// Assumes stdin/- is plain text unless specified elsewhere
+    ///
+    #[tracing::instrument]
+    fn from(value: T) -> Self {
+        // all lowercase
+        //
+        let p: PathBuf = value.clone().into();
+        let p1 = value.clone().into().to_string_lossy().to_ascii_lowercase();
+
+        let ext = match p.extension() {
+            Some(ext) => ext,
+            _ => OsStr::new("txt"),
+        }
+        .to_string_lossy()
+        .to_ascii_lowercase();
+
+        if ext == "gz" {
+            // Check for `.tar.gz`
+            if p1.ends_with(".tar.gz") || p1.ends_with(".tgz") {
+                return Input::TarGzip;
+            }
+        }
+        // Match the rest
+        //
+        match ext.as_ref() {
+            "csv" => Input::Csv,
+            "zip" => Input::Zip,
+            "txt" => Input::Xml,
+            "xml" => Input::Xml,
+            "tgz" => Input::TarGzip,
+            "gz" => Input::Gzip,
+            _ => Input::Unknown,
+        }
     }
 }
 
-/// Validate the input type.
-///
-/// Our currently supported file types:
-///
-/// - CSV
-/// - GZIP
-/// - Zip
-/// - XML
-/// - Plain text (aka invalid)
-///
-/// Example:
-/// ```rust
-/// # use dmarc_rs::filetype::*;
-/// let inp = valid_input("plain").unwrap();
-/// assert_eq!(Input::Plain, inp);
-/// ```
-///
-pub fn valid_input(itype: &str) -> Result<Input> {
-    return match itype.to_lowercase().as_str() {
-        "csv" => Ok(Input::Csv),
-        "plain" => Ok(Input::Plain),
-        "txt" => Ok(Input::Plain),
-        "gzip" => Ok(Input::Gzip),
-        "gz" => Ok(Input::Gzip),
-        "xml" => Ok(Input::Xml),
-        "zip" => Ok(Input::Zip),
-        _ => Err(anyhow!("Invalid type")),
-    };
-}
-
-/// Matches a filename to a given input type based on the extension.
-/// Assumes stdin/- is plain text unless specified elsewere
-///
-pub fn ext_to_ftype(p: &str) -> Input {
-    let p = PathBuf::from(p);
-    let ext = match p.extension() {
-        Some(ext) => ext,
-        _ => OsStr::new("txt"),
-    }
-    .to_string_lossy()
-    .to_ascii_lowercase();
-
-    match ext.as_str() {
-        "csv" => Input::Csv,
-        "zip" => Input::Zip,
-        "txt" => Input::Plain,
-        "xml" => Input::Xml,
-        "gz" => Input::Gzip,
-        _ => Input::Plain,
+impl Input {
+    pub fn valid(self) -> bool {
+        self != Input::Unknown
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use rstest::rstest;
-    use std::path::PathBuf;
+
+    use super::*;
 
     #[rstest]
-    #[case("foo", Input::Plain)]
-    #[case("foo.txt", Input::Plain)]
+    #[case("foo", Input::Xml)]
+    #[case("foo.txt", Input::Xml)]
     #[case("foo.zip", Input::Zip)]
     #[case("foo.ZIP", Input::Zip)]
     #[case("foo.gz", Input::Gzip)]
@@ -119,40 +114,31 @@ mod tests {
     #[case("foo.XML", Input::Xml)]
     #[case("foo.csv", Input::Csv)]
     #[case("foo.CSV", Input::Csv)]
-    #[case(".CSV", Input::Plain)]
-    fn test_ext_to_ftype(#[case] f: PathBuf, #[case] t: Input) {
-        let p = PathBuf::from(f);
-        assert_eq!(t, ext_to_ftype(&p))
+    #[case("foo.tar.GZ", Input::TarGzip)]
+    #[case("foo.tGZ", Input::TarGzip)]
+    #[case("foo.bar", Input::Unknown)]
+    fn test_input_from_str(#[case] f: &str, #[case] t: Input) {
+        assert_eq!(t, Input::from(f))
     }
 
     #[rstest]
-    #[case("plain", Input::Plain)]
-    #[case("TXT", Input::Plain)]
-    #[case("gzip", Input::Gzip)]
-    #[case("gz", Input::Gzip)]
-    #[case("zip", Input::Zip)]
-    #[case("Zip", Input::Zip)]
-    fn test_valid_input_ok(#[case] s: &str, #[case] it: Input) {
-        let r = valid_input(s);
-        assert!(valid_input(s).is_ok());
-        let r = match r {
-            Ok(r) => r,
-            Err(_) => Input::Plain,
-        };
-        assert_eq!(it, r);
-    }
-
-    #[rstest]
-    #[case("")]
-    #[case("qZip")]
-    #[case("")]
-    fn test_valid_input_nok(#[case] s: &str) {
-        let r = valid_input(s);
-        assert!(valid_input(s).is_err());
-        let r = match r {
-            Ok(_) => "bad ok".to_string(),
-            Err(e) => e.to_string(),
-        };
-        assert_eq!("Invalid type", r);
+    #[case("foo", Input::Xml)]
+    #[case("foo.txt", Input::Xml)]
+    #[case("foo.zip", Input::Zip)]
+    #[case("foo.ZIP", Input::Zip)]
+    #[case("foo.gz", Input::Gzip)]
+    #[case("foo.GZ", Input::Gzip)]
+    #[case("foo.Gz", Input::Gzip)]
+    #[case("foo.xml", Input::Xml)]
+    #[case("foo.XML", Input::Xml)]
+    #[case("foo.csv", Input::Csv)]
+    #[case("foo.CSV", Input::Csv)]
+    #[case(".CSV", Input::Unknown)]
+    #[case("foobar", Input::Unknown)]
+    #[case("qZip", Input::Unknown)]
+    #[case("", Input::Unknown)]
+    fn test_valid_input(#[case] s: &str, #[case] it: Input) {
+        let r = Input::from(s);
+        assert!(r.valid());
     }
 }
