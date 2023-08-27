6134526a100bd3d77dcec5972276b8b00505905f
Ollivier Robert <roberto@keltia.net>
2023-08-27 23:33:53 +0200

    rustfmt: reorder.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 //! This crate implement the library part of `dmarc-rs`, dealing with IPs and list of IPs
 //!
 
-mod entry;
-mod filetype;
-mod res;
-mod task;
-mod types;
-
+pub use dmarc::*;
 pub use entry::*;
 pub use filetype::*;
 pub use res::*;
-pub use task::*;
-pub use types::*;
+
+mod dmarc;
+mod entry;
+mod filetype;
+mod res;
