a7489f0406872d2633aeae38bba5c136b4764247
Ollivier Robert <roberto@keltia.net>
2025-02-22 00:19:21 +0100

    Upgrade to assert_cmd v2.1 API.

--- tests/cli-files.rs
+++ tests/cli-files.rs
@@ -1,9 +1,8 @@
-use assert_cmd::Command;
-
-const BIN: &str = "dmarc-cat";
+use assert_cmd::prelude::*;
+use std::process::Command;
 
 #[test]
 fn test_unknown_file() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
     cmd.arg("/nonexistent").assert().failure();
 }
--- tests/cli-opts.rs
+++ tests/cli-opts.rs
@@ -1,34 +1,33 @@
-use assert_cmd::Command;
-
-const BIN: &str = "dmarc-cat";
+use assert_cmd::prelude::*;
+use std::process::Command;
 
 #[test]
 fn test_empty_args() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
     cmd.assert().failure();
 }
 
 #[test]
 fn test_help() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
     cmd.arg("-h").assert().success();
 }
 
 #[test]
 fn test_version() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
 
     cmd.arg("-V").assert().success();
 }
 
 #[test]
 fn test_invalid_type_nok() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
     cmd.arg("-t").arg("blah").assert().failure();
 }
 
 #[test]
 fn test_invalid_type_ok() {
-    let mut cmd = Command::cargo_bin(BIN).unwrap();
+    let mut cmd = Command::new(std::env::var("CARGO_BIN_EXE_dmarc-rs").unwrap());
     cmd.arg("-t").arg("csv").assert().failure();
 }
